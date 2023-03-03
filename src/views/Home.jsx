91f94152447fd2ebc3a74f69a04136de0560de85
Alaa Aldandachi <alaa.aldandachi@cleura.com>
2023-03-03 16:50:16 +0100

    Added Home page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -6,8 +6,12 @@ import ToggleNavButton from "./../components/ToggleNavButton"
 function Home() {
 
     return (
-        <section>
+        <section className="home">
             <ToggleNavButton />
+            <figure className="home__logo"></figure>
+            <h1 className="home__h1">AIR BEAN</h1>
+            <p className="home__p">DRONEDELIVERED COFFEE</p>
+            <p className="home__group home__bounce">Smart Gurdians</p>
         </section>
     )
 }
