c9e9fe68a0a354005a0ba8f3d2acaf842315474f
Alaa Aldandachi <58466561+AlaaDan@users.noreply.github.com>
2023-03-04 11:13:13 +0100

    Added Link from Home page to Menu page
    Now the logo is animated and when you click on it, it will go the Menu page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,8 @@
 import "./Home.scss"
 
 import ToggleNavButton from "./../components/ToggleNavButton"
+import { Link } from 'react-router-dom'
+
 
 
 function Home() {
@@ -8,10 +10,10 @@ function Home() {
     return (
         <section className="home">
             <ToggleNavButton />
-            <figure className="home__logo"></figure>
+            <Link to="/Menu" className="home__logo home__bounce"></Link>
             <h1 className="home__h1">AIR BEAN</h1>
             <p className="home__p">DRONEDELIVERED COFFEE</p>
-            <p className="home__group home__bounce">Smart Gurdians</p>
+            <p className="home__group ">Smart Gurdians</p>
         </section>
     )
 }
