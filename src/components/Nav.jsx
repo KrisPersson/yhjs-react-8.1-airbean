8ff56293074daa3a38acfcec719736fdf949fbf0
KrisPersson <112572136+KrisPersson@users.noreply.github.com>
2023-03-09 11:44:36 +0100

    Little fixes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,9 +11,9 @@ function Nav() {
         <div className="nav">
             <Link to="/Menu" className="navLink"><h2>Meny</h2></Link>
             <Link to="/About" className="navLink" ><h2>Vårt kaffe</h2></Link>
-            { loginStatus === true ?
+            { loginStatus ?
                 <Link to="/Profile" className="navLink" ><h2>Min profil</h2></Link> :
-                <Link to="/Profile" className="navLink" ><h2>Registrera konto</h2></Link> }
+                <Link to="/Profile" className="navLink" ><h2>Logga in</h2></Link> }
             <Link to="/Status" className="navLink" ><h2>Orderstatus</h2></Link>
         </div>
     )
